3e7fb0e700808c03f51eefa8454d45c9936f8c15
Fabricio Valadares <fabricio.valadares@gmail.com>
2024-09-10 21:36:48 -0300

    Questao 15 A anterior era 19 :D

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,58 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
         CaixaEletronico caixaEletronico = new CaixaEletronico(100, input);
+        int opcao;
 
 //        double resultado = caixaEletronico.calculaDobro(100);
 
 //        System.out.println("O resultado foi: "  + resultado);
+        while (true) {
+            System.out.println("19 - Questao 19\n" +
+                    "15 - Questao 15\n" +
+                    "0 - Sair\n");
+            System.out.print("Sua escolha:> ");
+            opcao = input.nextInt();
 
-        caixaEletronico.exibirMenu();
+            switch (opcao) {
+                case 0:
+                    System.out.println("SAINDO... \nATE A PROXIMA\n");
+                    return;
+                case 19:
+                    caixaEletronico.exibirMenu();
+                    break;
+                case 15:
+                    Ponto p1;
+                    Ponto p2;
+                    double x;
+                    double y;
+
+                    System.out.println("INFORME A COORDENADA X DO PONTO 1");
+                    x = input.nextDouble();
+                    System.out.println("INFORME A COORDENADA Y DO PONTO 1");
+                    y = input.nextDouble();
+
+                    p1 = new Ponto(x, y);
+
+                    System.out.println("INFORME A COORDENADA X DO PONTO 2");
+                    x = input.nextDouble();
+                    System.out.println("INFORME A COORDENADA Y DO PONTO 2");
+                    y = input.nextDouble();
+
+                    p2 = new Ponto(x, y);
+
+                    System.out.println();
+
+                    System.out.println("A DISTANCIA DO " + p1 +
+                            " PARA O " + p2 + " EH: "
+                            + p1.calcularDistanciaParaPonto2(p2));
+
+
+                    break;
+                default:
+                    break;
+            }
+        }
+
 
     }
 }
--- src/Ponto.java
+++ src/Ponto.java
@@ -1,12 +1,31 @@
 
 public class Ponto {
-    private double pontoX; // Coordenada X
-    private double pontoY; // Coordenada Y
+    private double x; // Coordenada X
+    private double y; // Coordenada Y
 
     // Construtor da classe
-    public Ponto(double pontoX,
-                 double pontoY) {
-        this.pontoX = pontoX;
-        this.pontoY = pontoY;
+    public Ponto(double x,
+                 double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double calcularDistanciaParaPonto2(Ponto ponto2) {
+        double distancia;
+
+        distancia = Math.sqrt(
+                Math.pow((this.x - ponto2.x), 2) +
+                        Math.pow((this.y - ponto2.y), 2)
+        );
+
+        return distancia;
+    }
+
+    @Override
+    public String toString() {
+        return "Ponto(" +
+                x +
+                ", " + y +
+                ')';
     }
 }
