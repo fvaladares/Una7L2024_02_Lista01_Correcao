8a8b06aced3b3e53f57285f1b503f4b8934b9dcd
Fabricio Valadares <fabricio.valadares@gmail.com>
2024-09-10 21:10:25 -0300

    Questao 15

---
+++ src/Ponto.java
@@ -0,0 +1,12 @@
+
+public class Ponto {
+    private double pontoX; // Coordenada X
+    private double pontoY; // Coordenada Y
+
+    // Construtor da classe
+    public Ponto(double pontoX,
+                 double pontoY) {
+        this.pontoX = pontoX;
+        this.pontoY = pontoY;
+    }
+}
